c79cbc9f09a6d978e96ad9dcf36f081850d24961
Ry2X <45420571+ry2x@users.noreply.github.com>
2025-05-26 23:11:44 +0900

    feat: update Card component styles and integrate icon

--- src/components/home/Card.tsx
+++ src/components/home/Card.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { ArrowLongRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
 interface CardProps {
@@ -12,12 +13,12 @@ export default function Card({ title, description, href }: CardProps) {
   return (
     <Link
       href={href}
-      className="group p-8 bg-linear-to-br from-white/80 to-blue-50/80 dark:from-gray-800/80 dark:to-blue-900/80 rounded-2xl shadow-lg hover:shadow-xl transition-all duration-200 backdrop-blur-sm border border-white/20 dark:border-blue-900/20"
+      className="base-card group p-8 rounded-2xl shadow-lg hover:shadow-xl duration-200 border hover:scale-105"
     >
       <h2 className="text-2xl font-bold text-gray-900 dark:text-white mb-4">
         {title}
-        <span className="inline-block ml-2 text-blue-500 group-hover:translate-x-1 transition-transform">
-          →
+        <span className="inline-block ml-2 align-middle text-blue-500 group-hover:translate-x-1 transition-transform hover:scale-105">
+          <ArrowLongRightIcon className="size-7" />
         </span>
       </h2>
       <p className="text-gray-600 dark:text-gray-300">{description}</p>
